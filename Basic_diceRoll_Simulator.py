691baba15e686dbd9c3b52b6c80963813339428f
omerarshadsiddiqui <82222976+omerarshadsiddiqui@users.noreply.github.com>
2022-09-25 16:40:24 +0500

    Update Basic_diceRoll_Simulator.py

--- Basic_diceRoll_Simulator.py
+++ Basic_diceRoll_Simulator.py
@@ -1,7 +1,8 @@
 import random
-
+Print('Dice Roll Simulator Welcomes :) \n')
 repeat = True
 while repeat:
     print('Rolling the dice')
     print(random.randint(1,6))
     repeat = ('y' or 'yes') in input("Type y or yes in input \n").lower()
+Print('End')
